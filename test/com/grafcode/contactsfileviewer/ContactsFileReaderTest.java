7853e7c287dbaa9dd6ad9b6180d85edb78b66ea8
TimGraf <tim-graf@comcast.net>
2014-01-26 10:04:02 -0800

    Tests
    add a unit test class

---
+++ test/com/grafcode/contactsfileviewer/ContactsFileReaderTest.java
@@ -0,0 +1,16 @@
+package com.grafcode.contactsfileviewer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class ContactsFileReaderTest {
+    
+	@Test
+    public void thisAlwaysPasses() {
+    }
+}
