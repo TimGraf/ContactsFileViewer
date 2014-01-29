e985f5f9ee0159f9e7689d147bd89d6c54052258
TimGraf <tim-graf@comcast.net>
2014-01-29 09:33:37 -0800

    Refactor reader
    refactored the reader, placed close in finally block

--- src/com/grafcode/contactsfileviewer/model/ContactsFileReader.java
+++ src/com/grafcode/contactsfileviewer/model/ContactsFileReader.java
@@ -9,29 +9,38 @@ import java.net.URISyntaxException;
 public class ContactsFileReader {
 	private static String VALUE_SEPERATOR = ",";
 	
-	private BufferedReader fileReader;
+	private String fileName;
 	
 	public ContactsFileReader(String fileName) throws IllegalStateException, IOException, URISyntaxException {
-		fileReader = new BufferedReader(new FileReader(fileName));
+		this.fileName = fileName;
 	}
 	
 	public ContactsModel buildContactsModelFromFile() throws Exception {
-		String        nextLine = fileReader.readLine(); // read header line but don't use it
-		ContactsModel model    = new ContactsModel();
+		BufferedReader fileReader = null;
+		ContactsModel  model = new ContactsModel();
 		
-		while ((nextLine = fileReader.readLine()) != null) {
-			String[] tokens  = nextLine.split(VALUE_SEPERATOR);
-            
-			if (tokens.length == 0) {
-				throw new Exception("Invalid CSV file.");
+		try {
+			fileReader = new BufferedReader(new FileReader(fileName));
+			
+			String nextLine   = fileReader.readLine(); // read header line but don't use it
+			
+			while ((nextLine = fileReader.readLine()) != null) {
+				String[] tokens  = nextLine.split(VALUE_SEPERATOR);
+	            
+				if (tokens.length == 0) {
+					throw new Exception("Invalid CSV file.");
+				}
+	            
+				Contact contact = Contact.createContactFromTokens(tokens);
+	            
+				model.addContact(contact);
 			}
-            
-			Contact contact = Contact.createContactFromTokens(tokens);
-            
-			model.addContact(contact);
+		} finally {
+			
+			if (fileReader != null) {
+				fileReader.close();
+			}
 		}
-		
-		fileReader.close();
 		
 		return model;
 	}
